7a4434f7273690ea9f971bc9ba9e11f12d9d3093
azezmohammed <muhamadaziz047@gmail.com>
2020-12-04 11:30:56 +0700

    Update

---
+++ Operator/src/com/dicoding/javafundamental/operator/OperatorUnary.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.operator;
+
+public class OperatorUnary {
+    public static void main (String[] args){
+        System.out.println("opertari unary plus");
+        int nilaiAwal = 5;
+        int jumlah = +nilaiAwal;
+        System.out.println("Testing" + jumlah);
+        System.out.println();
+    }
+}
