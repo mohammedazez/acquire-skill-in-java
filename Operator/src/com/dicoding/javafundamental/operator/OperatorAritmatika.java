b69ede142dc9d7d356bc571beb0bfe415992c2ca
azezmohammed <muhamadaziz047@gmail.com>
2020-12-04 11:22:48 +0700

    first commit

---
+++ Operator/src/com/dicoding/javafundamental/operator/OperatorAritmatika.java
@@ -0,0 +1,30 @@
+package com.dicoding.javafundamental.operator;
+
+public class OperatorAritmatika {
+    public static void main (String[] args){
+        System.out.println("Operasi penjumlahan");
+        int penjumlahan = 1 + 5;
+        System.out.println("Ini adalah hasil operasi penjumlahan " + penjumlahan);
+        System.out.println();
+
+        System.out.println("Operasi Pengurangan");
+        int hasilPengurangan = 4 - 1;
+        System.out.println("Hasil 4 - 1 = " + hasilPengurangan);
+        System.out.println();
+        System.out.println("Operasi Pengalian");
+        int hasilPengalian = 5 * 5;
+        System.out.println("Hasil 5 * 5 = " + hasilPengalian);
+        System.out.println();
+        System.out.println("Operasi Pembagian");
+        int hasilPembagian = 20 / 2;
+        System.out.println("Hasil 20 / 2 = " + hasilPembagian);
+        System.out.println();
+
+        System.out.println("Operasi Habis bagi");
+        int hasilSisa = 8 % 2;
+        System.out.println("Hasil 8 % 2 = " + hasilSisa);
+        int hasilSisaLain = 9 % 2;
+        System.out.println("Hasil 9 % 2 = " + hasilSisaLain);
+        System.out.println();
+    }
+}
