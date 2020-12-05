c5bf31c3f7be67d8b8e9d13ffb70062ca3b194e8
azezmohammed <muhamadaziz047@gmail.com>
2020-12-05 17:41:14 +0700

    Tambah file if then else

--- Percabangan/src/com/dicoding/javafundamental/percabangan/Switch.java
+++ Percabangan/src/com/dicoding/javafundamental/percabangan/Switch.java
@@ -1,6 +1,32 @@
 package com.dicoding.javafundamental.percabangan;
 
 public class Switch {
+    public static void main(String[] args){
+        int input = 6;
 
+        switch (input){
+            case 1:
+                System.out.println("Ini nomor 1");
+                break;
+            case 2:
+                System.out.println("Ini nomor 2");
+                break;
+            case 3:
+                System.out.println("Ini nomor 3");
+                break;
+            case 4:
+                System.out.println("Ini nomor 4");
+                break;
+            case 5:
+                System.out.println("Ini nomor 5");
+                break;
+            case 6:
+                System.out.println("Ini nomor 6");
+                break;
+            default:
+                break;
+
+        }
+    }
 }
 
