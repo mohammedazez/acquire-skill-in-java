f47b147d087a1a291b2e90397f04edfb3466b6a4
azezmohammed <muhamadaziz047@gmail.com>
2020-12-05 17:19:24 +0700

    Tambah file if then else

---
+++ Percabangan/src/com/dicoding/javafundamental/percabangan/IfThenElse.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental.percabangan;
+
+public class IfThenElse {
+    public static void main(String[] args) {
+        boolean lampu = false;
+
+        if(lampu){
+            System.out.println("Menayalakan lampu");
+        } else {
+            System.out.println("lampu tidak menyala");
+        }
+    }
+}
---
+++ Percabangan/src/com/dicoding/javafundamental/percabangan/IfThenElseIfThenElse.java
@@ -0,0 +1,4 @@
+package com.dicoding.javafundamental.percabangan;
+
+public class IfThenElseIfThenElse {
+}
