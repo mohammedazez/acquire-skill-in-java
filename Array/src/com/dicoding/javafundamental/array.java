f0bc8bb896f1a0e14fed38d4239f9f34126a9212
azezmohammed <muhamadaziz047@gmail.com>
2020-12-06 14:08:50 +0700

    add array

---
+++ Array/src/com/dicoding/javafundamental/array.java
@@ -0,0 +1,16 @@
+package com.dicoding.javafundamental;
+
+public class array {
+    public static void main(String[] args){
+        int[] arrint = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+        System.out.println(arrint[0]);
+        System.out.println(arrint[1]);
+        System.out.println(arrint[2]);
+        System.out.println(arrint[3]);
+        System.out.println(arrint[4]);
+        System.out.println(arrint[5]);
+        System.out.println(arrint[6]);
+        System.out.println(arrint[7]);
+    }
+}
