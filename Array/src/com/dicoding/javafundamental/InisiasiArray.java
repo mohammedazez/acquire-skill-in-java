0095efcc57db4d52daa5c60824cbae23a79cca53
azezmohammed <muhamadaziz047@gmail.com>
2020-12-07 08:08:06 +0700

    add array

---
+++ Array/src/com/dicoding/javafundamental/InisiasiArray.java
@@ -0,0 +1,21 @@
+package com.dicoding.javafundamental;
+
+public class InisiasiArray {
+    public static void main(String[] args){
+        int[] array = new int[6];
+
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        array[5] = 6;
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+        System.out.println(array[5]);
+    }
+}
