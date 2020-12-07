84cb86edc35bcf46aa17c9c48b02d0ae9235fc52
azezmohammed <muhamadaziz047@gmail.com>
2020-12-07 08:34:33 +0700

    add kuis

---
+++ Array/src/com/dicoding/javafundamental/Kuis1.java
@@ -0,0 +1,10 @@
+package com.dicoding.javafundamental;
+
+public class Kuis1 {
+    public static void main(String[] args){
+        String str = "91204";
+        str += 23;
+
+        System.out.print(str);
+    }
+}
