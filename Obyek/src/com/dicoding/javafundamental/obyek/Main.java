0e60bc4da7e8fa2968d8a42c5ab04c9f47b98fd9
azezmohammed <muhamadaziz047@gmail.com>
2020-12-07 09:14:12 +0700

    add obyek class

--- Obyek/src/com/dicoding/javafundamental/obyek/Main.java
+++ Obyek/src/com/dicoding/javafundamental/obyek/Main.java
@@ -2,8 +2,15 @@ package com.dicoding.javafundamental.obyek;
 
 public class Main {
     public static void main(String[] args){
-        // menciptakan object dengan nama 'obyekHewan'
-        Hewan obyekHewan = new Hewan();
-        obyekHewan.cetakNama("Elang");
+        Hewan Elang = new Hewan("Elang");
+        Hewan Kucing = new Hewan("Kucing");
+
+        Elang.beratHewan(5);
+        Elang.jumlahKakiHewan(2);
+        Elang.cetakHewan();
+
+        Kucing.beratHewan(7);
+        Kucing.jumlahKakiHewan(4);
+        Kucing.cetakHewan();
     }
 }
