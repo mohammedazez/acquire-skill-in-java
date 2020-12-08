e7a948b7aff15045812cb8050751b0ff64675621
azezmohammed <muhamadaziz047@gmail.com>
2020-12-08 08:51:38 +0700

    add properti and method

---
+++ PropertiDanMetode/src/com/dicoding/javafundamental/propertimetode/Hewan.java
@@ -0,0 +1,30 @@
+package com.dicoding.javafundamental.propertimetode;
+
+class Hewan {
+    // Properti atau fields
+
+    // Inisiasi dengan nilai inisial
+    double tinggi = 30;
+    double berat = 3;
+
+    // Inisiasi melalui konstruktor
+    int umur;
+
+
+    // Konstruktor dengan parameter
+    Hewan(int umur){
+        this.umur = umur;
+    }
+
+    void lari(){
+        System.out.println("Berjalan dengan sangat cepat");
+    }
+
+    void jalan(){
+        System.out.println("Berjalan dengan sangat pelan");
+    }
+
+    void makan(){
+        System.out.println("Makan dengan menggunakan mulut.....");
+    }
+}
