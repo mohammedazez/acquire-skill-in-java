55b1c4954af3a26bb851dcb26c69b9727e9442ee
azezmohammed <muhamadaziz047@gmail.com>
2020-12-10 22:48:32 +0700

    Update

--- AccessModifier/src/com/dicoding/javafundamental/accessmodifier/KelasA.java
+++ AccessModifier/src/com/dicoding/javafundamental/accessmodifier/KelasA.java
@@ -2,9 +2,15 @@ package com.dicoding.javafundamental.accessmodifier;
 
 public class KelasA {
     private int memberA = 5;
+
+    char memberB = 'A';
+    double memberC = 1.5;
+
     private int functionA() {
         return memberA;
     }
+
+
     int functionB() {
         // Pemanggilan private member dan private function
         int hasil = functionA() + memberA;
